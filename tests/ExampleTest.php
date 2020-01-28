7c3f78128475e2d2515c973c5b2b97a0faa9cdf5
Amir Mahdi Khorshidi <amkgplus@gmail.com>
2020-01-28 21:19:32 +0330

    Initial

---
+++ tests/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function a_test()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
